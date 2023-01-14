164f5bccafed492dcc2435f03b665264679e7ecf
Merve ÖZ <104096244+themervez@users.noreply.github.com>
2023-01-14 22:49:11 +0300

    Completed

---
+++ DockerNewConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DockerNewConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i<=1000)
+            {
+                Console.WriteLine(i + " "+ "Hello World");
+                i++;
+                System.Threading.Thread.Sleep(1000);//1 saniyelik aralıklarla çalıştır
+            }
+        }
+    }
+}
